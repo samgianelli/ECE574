f582e351c878e6f6e70cf6811ebe516e818e6876
samgianelli <samgianelli@gmail.com>
2017-09-23 19:29:57 -0700

    Updated Circuit 8

--- ECE574_HW1/ECE574_HW1.srcs/sources_1/new/Circuit8.v
+++ ECE574_HW1/ECE574_HW1.srcs/sources_1/new/Circuit8.v
@@ -20,7 +20,47 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Circuit8(
-
+module Circuit8(a, b, c, zero, z);
+    input [63:0] a, b, c, zero;
+    output [63:0] z;
+    
+    wire [63:0] e, f, g, zwire;
+    wire gEQz;
+    
+    DEC #(64) dec_1 (
+        .a(a),
+        .d(e)
     );
+    
+    INC #(64) inc_1 (
+        .a(c),
+        .d(f)
+    );
+    
+    MOD #(64) mod_1 (
+        .a(a),
+        .b(c),
+        .rem(g)
+    );
+    
+    COMP #(64) comp_1 (
+        .a(g),
+        .b(zero),
+        .eq(gEQz)
+    );
+    
+    MUX #(64) mux_1 (
+        .a(e),
+        .b(f),
+        .sel(gEQz),
+        .d(zwire)
+    );
+    
+    REG #(64) reg_1 (
+        .d(zwire),
+        .Clk(clk),
+        .Rst(rst),
+        q(z)
+    );
+    
 endmodule
