1f9db876603dddc8f5b5edec38e6123034960cac
diegojimenez <diegojimenez@email.arizona.edu>
2017-10-28 10:06:27 -0700

    Changed to use command arguments

--- ECE574_Assignment2/src/main.cpp
+++ ECE574_Assignment2/src/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char* argv[]) // Will add input arguments in at end of projec
 	}
 
 	//vector<string> circuits = { "474a_circuit1", "474a_circuit2", "474a_circuit3", "474a_circuit4", "474a_circuit5", "574a_circuit6", "574a_circuit7", "574a_circuit8" };
-	vector<string> circuits = { "mixedcircuit3" };
+	vector<string> circuits = { argv[1] };
 	map<string, vector<double>> m;
 	m["REG"] = { 2.616, 2.644, 2.879, 3.061, 3.602, 3.966 };
 	m["ADD"] = { 2.704, 3.713, 4.924, 5.638, 7.270, 9.566 };
@@ -41,9 +41,9 @@ int main(int argc, char* argv[]) // Will add input arguments in at end of projec
 		unsigned int i = 0;
 		vector<string> netlistContents;
 		TopModule *topModule = new TopModule();
-		netlistContents = readNetlist(circuitName + ".txt");
+		netlistContents = readNetlist(circuitName);
 		if (netlistContents.size() == 0) {
-			cout << "Cannot open file or empty file: " << circuitName << ".txt" << endl;
+			cout << "Cannot open file or empty file: " << circuitName << endl;
 			return 1;
 		}
 
@@ -79,7 +79,7 @@ int main(int argc, char* argv[]) // Will add input arguments in at end of projec
 		topModule->findCriticalPath();
 
 		// Write to the .v file
-		writeToFile(circuitName, topModule);
+		writeToFile(argv[2], topModule);
 
 	}
 }
@@ -105,9 +105,9 @@ vector<string> readNetlist(string fileName)
 
 void writeToFile(string circuitName, TopModule *topModule) {
 	ofstream circuitFile;
-	circuitFile.open(circuitName + ".v");
+	circuitFile.open(circuitName);
 	cout << endl << endl;
-	topModule->printModuleName(circuitFile, circuitName);
+	topModule->printModuleName(circuitFile, circuitName.substr(0, circuitName.size() - 2));
 	topModule->printInputs(circuitFile);
 	topModule->printOutputs(circuitFile);
 	topModule->printWires(circuitFile);
